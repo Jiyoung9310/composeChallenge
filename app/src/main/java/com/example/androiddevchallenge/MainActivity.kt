cd06ca72c5a2a35cf2f90e38fd1f3b697447451b
jiyoung <ji9310@naver.com>
2021-03-01 18:44:44 +0900

    add puppy name and age

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,21 +19,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -91,16 +92,24 @@ fun PuppyListItem(item: Puppy) {
             .padding(Dimens.PaddingNormal)
             .fillMaxWidth()
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Column {
-                Text(text = item.name, fontWeight = FontWeight.Bold)
-                Text(item.age)
-            }
-        }
+        PuppyNameAndAge(item)
         Spacer(modifier = Modifier.padding(Dimens.PaddingSmall))
-        Card {
-            PuppyImage(item.imageUrl)
+        PuppyImage(item.imageUrl)
+    }
+}
+
+@Composable
+private fun PuppyNameAndAge(item: Puppy) {
+    Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = item.name, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.padding(2.dp))
+            Image(painter = painterResource(if(item.sex == "male") R.drawable.ic_male else R.drawable.ic_female),
+                contentDescription = item.sex,
+                modifier = Modifier.size(15.dp)
+            )
         }
+        Text(item.age)
     }
 }
 
@@ -110,22 +119,24 @@ private fun PuppyImage(
     modifier: Modifier = Modifier,
     placeholderColor: Color = MaterialTheme.colors.onSurface.copy(0.2f)
 ) {
-    CoilImage(
-        data = imageUrl,
-        contentScale = ContentScale.Crop,
-        fadeIn = true,
-        contentDescription = null,
-        loading = {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(placeholderColor)
-            )
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .height(Dimens.cardHeight)
-    )
+    Card {
+        CoilImage(
+            data = imageUrl,
+            contentScale = ContentScale.Crop,
+            fadeIn = true,
+            contentDescription = null,
+            loading = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(placeholderColor)
+                )
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .height(Dimens.cardHeight)
+        )
+    }
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
